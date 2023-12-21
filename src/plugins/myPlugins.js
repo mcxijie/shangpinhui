7111eda5418f1a7d0eb90c84da45adedb52fd898
xijie <907725402@qq.com>
2023-12-21 14:31:03 +0800

    1.未提交的代码

---
+++ src/plugins/myPlugins.js
@@ -0,0 +1,7 @@
+let myPlugins = {};
+
+myPlugins.install = function () {
+    console.log("调用")
+}
+
+export default myPlugins;
---
+++ src/views/Center/groupOrder/index.vue
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<template>
+  <div>团购订单内容</div>
+</template>
+
+<style lang="less" scoped>
+
+</style>
